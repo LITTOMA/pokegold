ac9538c870409b650dd6e6e7151fde31c2ceb2f4
LIT <littoma@hotmail.com>
2026-06-13 01:23:27 +0800

    Fix Trainer Card badge page attrs

--- engine/menus/trainer_card.asm
+++ engine/menus/trainer_card.asm
@@ -98,6 +98,47 @@ TrainerCard_SetPage1Attrs:
 	call TrainerCard_SetPage1TextAttrs
 	jp TrainerCard_SetPage1ChineseAttrs
 
+TrainerCard_SetBadgePageAttrs:
+	call TrainerCard_SetTopAttrs
+	hlcoord 2, 10, wAttrmap
+	lb bc, 1, 16
+	call TrainerCard_SetTextAttrBox
+	hlcoord 2, 13, wAttrmap
+	lb bc, 1, 16
+	call TrainerCard_SetTextAttrBox
+	hlcoord 2, 11, wAttrmap
+	lb bc, 2, 4
+	ld a, $1 ; falkner
+	call TrainerCard_FillAttrBox
+	hlcoord 6, 11, wAttrmap
+	lb bc, 2, 4
+	ld a, $2 ; bugsy
+	call TrainerCard_FillAttrBox
+	hlcoord 10, 11, wAttrmap
+	lb bc, 2, 4
+	ld a, $3 ; whitney
+	call TrainerCard_FillAttrBox
+	hlcoord 14, 11, wAttrmap
+	lb bc, 2, 4
+	ld a, $4 ; morty
+	call TrainerCard_FillAttrBox
+	hlcoord 2, 14, wAttrmap
+	lb bc, 2, 4
+	ld a, $5 ; chuck
+	call TrainerCard_FillAttrBox
+	hlcoord 6, 14, wAttrmap
+	lb bc, 2, 4
+	ld a, $6 ; jasmine
+	call TrainerCard_FillAttrBox
+	hlcoord 10, 14, wAttrmap
+	lb bc, 2, 4
+	ld a, $7 ; pryce
+	call TrainerCard_FillAttrBox
+	hlcoord 14, 14, wAttrmap
+	lb bc, 2, 4
+	ld a, TRAINER_CARD_TEXT_ATTR ; clair
+	jp TrainerCard_FillAttrBox
+
 TrainerCard_SetTopTextAttrs:
 	hlcoord 6, 1, wAttrmap
 	lb bc, 1, 1
@@ -249,7 +290,7 @@ TrainerCard_Page2_LoadGFX:
 	ld b, SCGB_TRAINER_CARD
 	call GetSGBLayout
 	call SetDefaultBGPAndOBP
-	call TrainerCard_SetTopAttrs
+	call TrainerCard_SetBadgePageAttrs
 	call CGBOnly_CopyTilemapAtOnce
 	call TrainerCard_IncrementJumptable
 	ret
@@ -302,7 +343,7 @@ TrainerCard_Page3_LoadGFX:
 	ld b, SCGB_TRAINER_CARD
 	call GetSGBLayout
 	call SetDefaultBGPAndOBP
-	call TrainerCard_SetTopAttrs
+	call TrainerCard_SetBadgePageAttrs
 	call CGBOnly_CopyTilemapAtOnce
 	call TrainerCard_IncrementJumptable
 	ret
